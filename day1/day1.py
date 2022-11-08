f5ea7c4e75eefab51503b4c519c58889c37beed2
Tomas Peluritis <tomas1991@gmail.com>
2022-11-08 21:08:37 +0200

    small adjustments

--- day1/day1.py
+++ day1/day1.py
@@ -1,8 +1,6 @@
 import os
 from typing import List
-from numpy.lib.stride_tricks import sliding_window_view
 import numpy as np
-
 
 def get_input(file_name: str) -> List:
     with open(file_name, 'r') as f:
@@ -10,13 +8,11 @@ def get_input(file_name: str) -> List:
 
 def solution(list_of_ints:List, length:int)-> int:
     cnt = 0
-    zipped = sliding_window_view(np.array(list_of_ints), window_shape=length)[::1]
+    zipped = np.lib.stride_tricks.sliding_window_view(np.array(list_of_ints), window_shape=length)[::1]
     for i in range(zipped.shape[0]-1):
         if sum(zipped[i])<sum(zipped[i+1]):
             cnt+=1
-            
     return cnt
-
 
 def main():
     file_name=f'{os.getcwd()}/day1/input.txt'
